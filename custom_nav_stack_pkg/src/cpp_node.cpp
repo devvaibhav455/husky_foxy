6e6f5ae2a1d746d5060aae9c196bc1f0ba6d7024
devvaibhav455 <vaibhav.dev5@gmail.com>
2022-09-27 17:12:10 -0400

    ROS PointCloud2 to PCL Pointcloud successful

--- custom_nav_stack_pkg/src/cpp_node.cpp
+++ custom_nav_stack_pkg/src/cpp_node.cpp
@@ -1,17 +1,24 @@
 #include <memory>
+#include <iostream>
 
 #include "rclcpp/rclcpp.hpp"
 #include "std_msgs/msg/string.hpp"
 #include "sensor_msgs/msg/point_cloud2.hpp"
-// #include <pcl-1.10/pcl/point_types.h>
-// #include <pcl-1.10/pcl/PCLPointCloud2.h>
-// #include <pcl-1.10/pcl/conversions.h>
+#include <pcl/point_cloud.h>
+#include <pcl/point_types.h>
+#include <pcl/PCLPointCloud2.h>
+#include <pcl/conversions.h>
+#include <pcl_conversions/pcl_conversions.h>
+#include <pcl/visualization/cloud_viewer.h>
+#include <boost/foreach.hpp>
 
 
 
-using std::placeholders::_1;
 
-// colcon build --packages-select custom_nav_stack_pkg --symlink-install && source install/local_setup.bash && ros2 run custom_nav_stack_pkg cpp_executable
+// using std::placeholders::_1;
+
+// clear && colcon build --packages-select custom_nav_stack_pkg --symlink-install && source install/local_setup.bash && ros2 run custom_nav_stack_pkg cpp_executable
+// sudo chown -R dev /opt/ros/foxy/share/ #to remove the squiggle error in vscode
 
 class MinimalSubscriber : public rclcpp::Node
 {
@@ -20,7 +27,8 @@ class MinimalSubscriber : public rclcpp::Node
     MinimalSubscriber()
     : Node("minimal_subscriber")
     {
-      subscription_ = this->create_subscription<sensor_msgs::msg::PointCloud2>("velodyne_points", 10, std::bind(&MinimalSubscriber::topic_callback, this, _1));
+      subscription_ = this->create_subscription<sensor_msgs::msg::PointCloud2>
+      ("velodyne_points", 10, std::bind(&MinimalSubscriber::topic_callback, this, std::placeholders::_1));
     }
 
   private:
@@ -31,8 +39,32 @@ class MinimalSubscriber : public rclcpp::Node
       // RCLCPP_INFO(this->get_logger(), "I received the message"); //
 
       // Convert the sensor_msgs/PointCloud2 data to pcl/PointCloud
-      // pcl::PointCloud<pcl::PointXYZ> cloud;
-      // pcl::fromROSMsg (*input, cloud);
+      // pcl::PointCloud<pcl::PointXYZ> cloud; //Working, Reference: https://ros-developer.com/2017/02/23/converting-pcl-point-cloud-to-ros-pcl-cloud-message-and-the-reverse/
+      // pcl::fromROSMsg (*msg, cloud); //Working
+      pcl::PointCloud<pcl::PointXYZ> cloud;      
+      pcl::fromROSMsg (*msg, cloud);
+      RCLCPP_INFO(this->get_logger(), "I received the PCL message , height is: '%d'", cloud.height);
+      // cloud.points[:].x = 4;
+      std::cout << "Complete ROS cloud is: " << msg << std::endl;      
+      std::cout << "Complete PCL cloud is: " << cloud << std::endl;      
+      std::cout << "PCL 0 cloud is: " << cloud.points[0] << std::endl;      
+      std::cout << "PCL 15000 cloud is: " << cloud.points[15000] << std::endl;      
+      std::cout << "PCL 25000 cloud is: " << cloud.points[25000] << std::endl;      
+
+ 
+      // // loop through the array elements
+      // for (size_t i = 0; i < n; i++) {
+      //     std::cout << input[i] << ' ';
+      // }
+      //Reading xyz values from the point cloud
+      // BOOST_FOREACH (const pcl::PointXYZ& pt, cloud.points)
+      // printf ("\t(%f, %f, %f)\n", pt.x, pt.y, pt.z);
+      
+      //... populate cloud
+      // pcl::visualization::CloudViewer viewer ("Simple Cloud Viewer");
+      // viewer.showCloud (cloud.makeShared());
+      std::cin.ignore();
+      
     }
     rclcpp::Subscription<sensor_msgs::msg::PointCloud2>::SharedPtr subscription_;
 };
