9464c761c74aebdab190f732986babfbd245ec85
devvaibhav455 <vaibhav.dev5@gmail.com>
2022-09-28 17:23:36 -0400

    Voxelization successful but double free or corrution (out) error still present

--- custom_nav_stack_pkg/src/cpp_node.cpp
+++ custom_nav_stack_pkg/src/cpp_node.cpp
@@ -11,20 +11,18 @@
 #include <pcl_conversions/pcl_conversions.h>
 #include <pcl/visualization/cloud_viewer.h>
 #include <boost/foreach.hpp>
-
-
+#include <pcl/filters/voxel_grid.h>
 
 
 // using std::placeholders::_1;
 
 // clear && colcon build --packages-select custom_nav_stack_pkg --symlink-install && source install/local_setup.bash && ros2 run custom_nav_stack_pkg cpp_executable
-// sudo chown -R dev /opt/ros/foxy/share/ #to remove the squiggle error in vscode
+// sudo chown -R dev /opt/ros/foxy/share/ #to remove the squiggle error in vscode where "dev" is the username
 
 class MinimalSubscriber : public rclcpp::Node
 {
   public:
-     
-    MinimalSubscriber()
+    MinimalSubscriber()    //Constructor which has the same name as that of class followed by a parentheses. A constructor in C++ is a special method that is automatically called when an object of a class is created. 
     : Node("minimal_subscriber")
     {
       subscription_ = this->create_subscription<sensor_msgs::msg::PointCloud2>
@@ -41,15 +39,23 @@ class MinimalSubscriber : public rclcpp::Node
       // Convert the sensor_msgs/PointCloud2 data to pcl/PointCloud
       // pcl::PointCloud<pcl::PointXYZ> cloud; //Working, Reference: https://ros-developer.com/2017/02/23/converting-pcl-point-cloud-to-ros-pcl-cloud-message-and-the-reverse/
       // pcl::fromROSMsg (*msg, cloud); //Working
-      pcl::PointCloud<pcl::PointXYZ> cloud;      
-      pcl::fromROSMsg (*msg, cloud);
+      // RCLCPP_INFO(this->get_logger(), "I received the PCL message , height is: '%d'", cloud.height); //WORKING
+      // pcl::PCLPointCloud2::Ptr cloud;      
+      // pcl::fromROSMsg (*msg, cloud);
+      pcl::PCLPointCloud2 cloud;
+      pcl_conversions::toPCL(*msg, cloud);
+      // pcl::PointCloud<pcl::PointXYZ>::Ptr temp_cloud(new pcl::PointCloud<pcl::PointXYZ>);
+      // pcl::fromPCLPointCloud2(*cloud,*temp_cloud);
       RCLCPP_INFO(this->get_logger(), "I received the PCL message , height is: '%d'", cloud.height);
       // cloud.points[:].x = 4;
-      std::cout << "Complete ROS cloud is: " << msg << std::endl;      
-      std::cout << "Complete PCL cloud is: " << cloud << std::endl;      
-      std::cout << "PCL 0 cloud is: " << cloud.points[0] << std::endl;      
-      std::cout << "PCL 15000 cloud is: " << cloud.points[15000] << std::endl;      
-      std::cout << "PCL 25000 cloud is: " << cloud.points[25000] << std::endl;      
+      // std::cout << "Complete ROS cloud is: " << msg << std::endl;
+      // std::cout << "Complete ROS cloud type is: " << typeid(msg).name() << std::endl;
+      // std::cout << "Complete PCL cloud is: " << cloud << std::endl;      
+      // std::cout << "Complete PCL cloud type is: " << typeid(cloud).name() << std::endl;
+      // std::cout << "PCL cloud points type is: " << typeid(cloud.points).name() << std::endl;
+      // std::cout << "PCL 0 cloud is: " << cloud.points[0] << std::endl;      
+      // std::cout << "PCL 15000 cloud is: " << cloud.points[15000] << std::endl;      
+      // std::cout << "PCL 25000 cloud is: " << cloud.points[25000] << std::endl;      
 
  
       // // loop through the array elements
@@ -63,17 +69,44 @@ class MinimalSubscriber : public rclcpp::Node
       //... populate cloud
       // pcl::visualization::CloudViewer viewer ("Simple Cloud Viewer");
       // viewer.showCloud (cloud.makeShared());
-      std::cin.ignore();
       
+      std::cerr << "PointCloud before filtering: " << cloud.width * cloud.height 
+             << " data points (" << pcl::getFieldsList (cloud) << ")." << std::endl;
+
+      
+      // Declaring pointer for the PCL "cloud" 
+      pcl::PCLPointCloud2::Ptr cloudPtr (&cloud);
+
+      // define a new container for the data
+      // pcl::PCLPointCloud2::Ptr cloud_filtered (new pcl::PCLPointCloud2 ());
+
+      // define a voxelgrid
+      // pcl::VoxelGrid<pcl::PCLPointCloud2> voxelGrid;
+      // set input to cloud
+      // voxelGrid.setInputCloud(cloudPtr);
+      // set the leaf size (x, y, z)
+      // voxelGrid.setLeafSize(0.1, 0.1, 0.1);
+      // apply the filter to dereferenced cloudVoxel
+      // voxelGrid.filter(*cloud_filtered);   
+
+      // std::cerr << "PointCloud after filtering: " << cloud_filtered->width * cloud_filtered->height 
+      //  << " data points (" << pcl::getFieldsList (*cloud_filtered) << ")." << std::endl;
+
+      // delete cloudPtr;
+      std::cin.ignore();
+      // std::cerr << "Reached callback end" << std::endl;
+      // cloudPtr = NULL;
     }
     rclcpp::Subscription<sensor_msgs::msg::PointCloud2>::SharedPtr subscription_;
+
 };
 
 int main(int argc, char * argv[])
 {
-  std::cout << "Inside main function now";
+  std::cout << "Inside main function now" << std::endl;
   rclcpp::init(argc, argv);
   rclcpp::spin(std::make_shared<MinimalSubscriber>());
+  
   rclcpp::shutdown();
   return 0;
 }
